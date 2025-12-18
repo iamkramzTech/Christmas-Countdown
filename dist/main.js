5bb3365043841b084d6db3a00f9204076ab32417
iamKramzTech <83441254+iamkramzTech@users.noreply.github.com>
2025-12-18 22:31:30 +0800

    Logic: initial commit countdown timer logic

---
+++ dist/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { body } = document;
+const daysElement = document.getElementById("days");
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+function BeginChristmasCountdown() {
+    const now = new Date();
+    const currentMonth = now.getMonth() + 1;
+    const currentDay = now.getDate();
+    // Figure out the year that the next Christmas will occur on
+    let nextChristmasYear = now.getFullYear();
+    if (currentMonth === 12 && currentDay > 25) {
+        nextChristmasYear += 1;
+    }
+    const nextChristmasDate = `Dec 25, ${nextChristmasYear} 00:00:00`;
+    const ChristmasDate = new Date(nextChristmasDate).getTime();
+    // Find the distance between now and the Christmas date
+    let distance = ChristmasDate - now;
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    //Don't calculate the distance if it is Christmas day
+    if (currentMonth !== 12 || (currentMonth === 12 && currentDay !== 25)) {
+        // Time calculations for days, hours, minutes and seconds
+        days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }
+    daysElement.textContent = days < 10 ? `0${days}` : days;
+    hoursElement.textContent = hours < 10 ? `0${hours}` : hours;
+    minutesElement.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    secondsElement.textContent = seconds < 10 ? `0${seconds}` : seconds;
+}
+setInterval(BeginChristmasCountdown, 1000);
