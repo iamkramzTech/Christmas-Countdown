a221fa9ecaa1166502d0d172f8706ee7c110ed6d
iamKramzTech <83441254+iamkramzTech@users.noreply.github.com>
2025-12-20 18:54:40 +0800

    fix: use Math.Ceil for days to round up

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ function BeginChristmasCountdown() {
     //Don't calculate the distance if it is Christmas day
     if (currentMonth !== 12 || (currentMonth === 12 && currentDay !== 25)) {
         // Time calculations for days, hours, minutes and seconds
-        days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        days = Math.ceil(distance / (1000 * 60 * 60 * 24));
         hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         seconds = Math.floor((distance % (1000 * 60)) / 1000);
